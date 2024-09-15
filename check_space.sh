32419651bdc3133cd6f3cef647f74668916f4ab7
Ilya Kochetkov <ie.kochetkov@yandex.ru>
2024-09-15 10:11:20 +0300

    Check space updated

--- check_space.sh
+++ check_space.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-REQUIRED_SPACE=$(du -s "$PGDATA" | awk '{print $1}')
-df "$PGDATA" | awk -v required_space="$REQUIRED_SPACE" 'NR==2 {if($4 < required_space) exit 1; exit 0}'
+REQUIRED_SPACE_MB=$(( $(du -s "$PGDATA" | awk '{print $1}') / 1024 ))
+AVAILABLE_SPACE_MB=$(( $(df "$PGDATA" | awk 'NR==2 {print $4}') / 1024 ))
+
+if [ "$AVAILABLE_SPACE_MB" -lt "$REQUIRED_SPACE_MB" ]; then
+    echo "Insufficient free space. Required: $REQUIRED_SPACE_MB MB, Available: $AVAILABLE_SPACE_MB MB."
+    exit 1
+else
+    exit 0
+fi
