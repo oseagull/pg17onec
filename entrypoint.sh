7f898dd88a2ea855be1d01e2385f1007f11bc478
Ilya Kochetkov <ie.kochetkov@yandex.ru>
2024-10-17 09:02:34 +0300

    Вроде запуск двух процессов

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,37 +2,66 @@
 
 set -e
 
-if [ "$1" = './postgres' ]; then
+terminate_processes() {
 
-    if [ ! -s "$PGDATA/PG_VERSION" ]; then
-        chown -R postgres:postgres "$PGDATA"
+    echo "Stopping postgres..."
+    postgres_pid=$(ps -eo pid,cmd | grep '/opt/pgpro/1c-16/bin/postgres' | grep -v grep | awk '{print $1}')
+    kill -SIGTERM "$postgres_pid" &
 
-        if [ -n "$PG_PASSWORD" ]; then
-            gosu postgres sh -c 'echo "$PG_PASSWORD" > /tmp/password_file'
-            gosu postgres ./initdb --pwfile=/tmp/password_file
-        else
-            gosu postgres ./initdb
-        fi
+    echo "Stopping pgagent..."
+    kill -SIGTERM "$pgagent_pid"
 
-        echo "synchronous_commit = off" >> "$PGDATA/postgresql.conf"
-        echo "unix_socket_directories = '/tmp,$PGSOCKET'" >> "$PGDATA/postgresql.conf"
-        # listen all network addresses
-        echo "listen_addresses = '*'" >> "$PGDATA/postgresql.conf"
-        # enable login-password auth for all
-        echo "host  all  all  0.0.0.0/0  md5" >> "$PGDATA/pg_hba.conf"
-        # need to set default password after start
+    wait "$postgres_pid"
+    echo "Processes stopped."
+    exit 0
+}
+
+trap 'terminate_processes' SIGTERM SIGINT
+
+initialize_database() {
+    # Early return if the database is already initialized
+    if [ -s "$PGDATA/PG_VERSION" ]; then
+        return
     fi
-
-    if [ ! -d "$PGSOCKET" ]; then
-        mkdir -p $PGSOCKET
+    chown -R postgres:postgres "$PGDATA"
+    if [ -n "$PG_PASSWORD" ]; then
+        gosu postgres sh -c 'echo "$PG_PASSWORD" > /tmp/password_file'
+        gosu postgres ./initdb --pwfile=/tmp/password_file
+    else
+        gosu postgres ./initdb
     fi
+    echo "synchronous_commit = off" >> "$PGDATA/postgresql.conf"
+    echo "unix_socket_directories = '/tmp,$PGSOCKET'" >> "$PGDATA/postgresql.conf"
+    echo "listen_addresses = '*'" >> "$PGDATA/postgresql.conf"
+    echo "host  all  all  0.0.0.0/0  md5" >> "$PGDATA/pg_hba.conf"
+    cat /pgdefault.conf >> /var/lib/1c/pgdata/postgresql.conf
+    rm /pgdefault.conf
+}
+
+initialize_socket() {
+    # Early return if the socket directory already exists
+    if [ -d "$PGSOCKET" ]; then
+        chown postgres $PGSOCKET
+        return
+    fi
+    mkdir -p $PGSOCKET
     chown postgres $PGSOCKET
+}
 
-    # gosu postgres pgagent host=$PGSOCKET dbname=postgres &
+start_processes() {
+    echo "Staring pgagent..."
+    gosu postgres pgagent -f host=$PGSOCKET dbname=postgres &
+    pgagent_pid=$!
+    echo "Starting postgres..."
+    gosu postgres ./postgres &
+    postgres_pid=$!
+    wait "$postgres_pid"
+}
 
-    echo "Statring postgres..."
-    exec gosu postgres ./postgres
-
+if [ "$1" = './postgres' ]; then
+    initialize_database
+    initialize_socket
+    start_processes
 fi
 
 exec "$@"
